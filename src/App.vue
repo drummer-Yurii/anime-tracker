17733673a12f314ab02f9f5b7ab1456273bbdbf9
Yurii Rybachok <drummer2009@ukr.net>
2023-11-12 11:09:50 +0200

    create form with input

--- src/App.vue
+++ src/App.vue
@@ -7,58 +7,67 @@ const search_results = ref([]);
 
 const my_anime_asc = computed(() => {
   return my_anime.value.sort((a, b) => {
-    return a.title.localeCompare(b, title)
-  })
-})
+    return a.title.localeCompare(b, title);
+  });
+});
 
 const searchAnime = () => {
-  const url = `https://api.jikan.moe/v4/anime?q=${query.value}`
+  const url = `https://api.jikan.moe/v4/anime?q=${query.value}`;
   fetch(url)
-    .then(res => res.json())
-    .then(res => {
-      search_results.value = res.data
-    })
-}
+    .then((res) => res.json())
+    .then((res) => {
+      search_results.value = res.data;
+    });
+};
 
-const handleInput = e => {
+const handleInput = (e) => {
   if (!e.target.value) {
-    search_results.value = []
+    search_results.value = [];
   }
-}
+};
 
-const addAnime = anime => {
-  search_results.value = []
-  query.value = ''
+const addAnime = (anime) => {
+  search_results.value = [];
+  query.value = '';
 
   my_anime.value.push({
     id: anime.mal_id,
     title: anime.title,
     image: anime.images.jpg.image_url,
     total_episodes: anime.episodes,
-    watched_episodes: 0
-  })
+    watched_episodes: 0,
+  });
 
-  localStorage.setItem('my_anime', JSON.stringify(my_anime.value))
-}
+  localStorage.setItem('my_anime', JSON.stringify(my_anime.value));
+};
 
-const increaseWatch = anime => {
-  anime.watched_episodes++
-  localStorage.setItem('my_anime', JSON.stringify(my_anime.value))
-}
-const decreaseWatch = anime => {
-  anime.watched_episodes--
-  localStorage.setItem('my_anime', JSON.stringify(my_anime.value))
-}
+const increaseWatch = (anime) => {
+  anime.watched_episodes++;
+  localStorage.setItem('my_anime', JSON.stringify(my_anime.value));
+};
+const decreaseWatch = (anime) => {
+  anime.watched_episodes--;
+  localStorage.setItem('my_anime', JSON.stringify(my_anime.value));
+};
 
 onMounted(() => {
-  my_anime.value = JSON.parse(localStorage.getItem('my_anime')) || [] 
-})
+  my_anime.value = JSON.parse(localStorage.getItem('my_anime')) || [];
+});
 </script>
 
 <template>
-  <main>Hello, World</main>
+  <main>
+    <h1>My Anime Tracker</h1>
+    <form @submit.prevent="searchAnime">
+      <input
+        type="text"
+        placeholder="Search for an anime..."
+        v-model="query"
+        @input="handleInput"
+      />
+      <button type="submit">Search</button>
+    </form>
+  </main>
 </template>
 
-<style>
-
-</style>
+<style></style>
