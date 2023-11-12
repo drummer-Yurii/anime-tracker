a1e75763e6223200084e6c6f77604695e21bbff8
Yurii Rybachok <drummer2009@ukr.net>
2023-11-12 12:11:05 +0200

    add styling for project

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ const search_results = ref([]);
 
 const my_anime_asc = computed(() => {
   return my_anime.value.sort((a, b) => {
-    return a.title.localeCompare(b, title);
+    return a.title.localeCompare(b.title);
   });
 });
 
@@ -65,7 +65,7 @@ onMounted(() => {
         v-model="query"
         @input="handleInput"
       />
-      <button type="submit">Search</button>
+      <button type="submit" class="button">Search</button>
     </form>
     <div class="results" v-if="search_results.length > 0">
       <div class="result" v-for="anime in search_results" :key="anime">
@@ -74,7 +74,7 @@ onMounted(() => {
           <h3>{{ anime.title }}</h3>
           <p :title="anime.synopsis" v-if="anime.synopsis">{{ anime.synopsis.slice(0, 120) }}...</p>
           <span class="flex-1"></span>
-          <button @click="addAnime(anime)">Add to my anime</button>
+          <button @click="addAnime(anime)" class="button">Add to my anime</button>
         </div>
       </div>
     </div>
@@ -88,12 +88,14 @@ onMounted(() => {
         <button
           v-if="anime.total_episodes !== anime.watched_episodes"
           @click="increaseWatch(anime)"
+          class="button"
         >
           +
         </button>
         <button
           v-if="anime.watched_episodes > 0"
           @click="decreaseWatch(anime)"
+          class="button"
         >
           -
         </button>
@@ -102,4 +104,159 @@ onMounted(() => {
   </main>
 </template>
 
-<style></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', sans-serif;
+}
+
+body {
+  background-color: #EEE;
+}
+
+main {
+  margin: 0 auto;
+  max-width: 768px;
+  padding: 1.5rem;
+}
+
+h1 {
+  text-align: center;
+  margin-bottom: 1.5rem;
+}
+
+form {
+  display: flex;
+  max-width: 480px;
+  margin: 0 auto 1.5rem;
+}
+
+form input {
+  appearance: none;
+  outline: none;
+  border: none;
+  background: white;
+
+  display: block;
+  color: #888;
+  font-size: 1.125rem;
+  padding: 0.5rem 1rem;
+  width: 100%;
+}
+
+.button {
+  appearance: none;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  background: none;
+
+  display: block;
+  padding: 0.5rem 1rem;
+  background-image: linear-gradient(to right, deeppink 50%, darkviolet 50%);
+  background-size: 200%;
+  color: #FFF;
+  font-size: 1.125rem;
+  font-weight: 700;
+  text-transform: uppercase;
+  transition: 0.4s;
+}
+
+.button:hover {
+  background-position: right;
+}
+
+.results {
+  background-color: #FFF;
+  border-radius: 0.5rem;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  max-height: 480px;
+  overflow-y: scroll;
+  margin-bottom: 1.5rem;
+}
+
+.result {
+  display: flex;
+  margin: 1rem;
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 0.5rem;
+  transition: 0.4s;
+}
+
+.result img {
+  width: 100px;
+  border-radius: 1rem;
+  margin-right: 1rem;
+}
+
+.details {
+  flex: 1 1 0%;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+}
+
+.flex-1 {
+  flex: 1 1 0%;
+}
+
+.details h3 {
+  font-size: 1.25rem;
+  margin-bottom: 0.5rem;
+}
+
+.details p {
+  font-size: 0.875rem;
+  line-height: 1.4;
+  margin-bottom: 0.5rem;
+}
+
+.details .button {
+  margin-left: auto;
+}
+
+.myanime h2 {
+  color: #888;
+  font-weight: 400;
+  margin-bottom: 1.5rem;
+}
+
+.myanime .anime {
+  display: flex;
+  align-items: center;
+  margin-bottom: 1.5rem;
+  background-color: #fff;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.anime img {
+  width: 72px;
+  height: 72px;
+  object-fit: cover;
+  border-radius: 1rem;
+  margin-right: 1rem;
+}
+
+.anime h3 {
+  color: #888;
+  font-size: 1.125rem;
+}
+
+.anime .episodes {
+  margin-right: 1rem;
+  color: #888;
+}
+
+.anime .button:first-of-type {
+  margin-right: 1rem;
+}
+
+.anime .button:last-of-type {
+  margin-right: 0;
+}
+</style>
